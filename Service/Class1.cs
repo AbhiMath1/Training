585c39a56e9f9201b3f36bd15a27901aaa92ecd6
Laxman Santosh <sanmca4u@gmail.com>
2020-06-09 07:59:02 +0530

    Add project files.

---
+++ Service/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Service
+{
+    public class Class1
+    {
+        public void PerformSearch(int x, int y)
+        {
+            Console.WriteLine("Found " + x);
+            Console.ReadKey();
+        }
+    }
+}
